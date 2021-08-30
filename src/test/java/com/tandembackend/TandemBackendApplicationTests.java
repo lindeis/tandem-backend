44dcae2ff0ce088dcb3630f3de5fb41972aee76d
rolandcsa <roland.csatlos8@protonmail.com>
2021-08-30 10:28:58 +0200

    Project Setup

---
+++ src/test/java/com/tandembackend/TandemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tandembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TandemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
