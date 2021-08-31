5dfbdda1cb94f7297f96de737d6d49e7d1f08cec
nybah <botosarpad@gmail.com>
2021-08-31 12:09:18 +0200

    Added database connector and a test class 'User'.

---
+++ src/main/java/com/tandembackend/model/User.java
@@ -0,0 +1,15 @@
+package com.tandembackend.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    private String name;
+    private String password;
+}
