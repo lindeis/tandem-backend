0a6cabde891edc0eb236b4861bea500157e84baa
nybah <botosarpad@gmail.com>
2021-09-02 12:04:26 +0200

    Readded password encoding at registration (deleted in last merge).

--- src/main/java/com/tandembackend/user/UserService.java
+++ src/main/java/com/tandembackend/user/UserService.java
@@ -72,7 +72,7 @@ public class UserService {
     }
 
     public User registerUser(RegisterRequestDTO registerRequestDTO) throws UsernameTakenException {
-        User u = new User(registerRequestDTO.getUsername(), registerRequestDTO.getPassword());
+        User u = new User(registerRequestDTO.getUsername(), passwordEncoder.encode(registerRequestDTO.getPassword()));
         if (!userRepository.findUserByUsername(u.getUsername()).isEmpty()) {
             throw new UsernameTakenException("The username " + u.getUsername() + " is already taken.");
         }
