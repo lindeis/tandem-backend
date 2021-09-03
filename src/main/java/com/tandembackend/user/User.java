ed869a635baa596ea26b9605db1e64148fddae3c
nybah <botosarpad@gmail.com>
2021-09-03 13:56:38 +0200

    Added room creation and room listing.

--- src/main/java/com/tandembackend/user/User.java
+++ src/main/java/com/tandembackend/user/User.java
@@ -1,11 +1,12 @@
 package com.tandembackend.user;
 
+import com.tandembackend.lobby.Room;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -16,4 +17,14 @@ public class User {
     @Id
     private String username;
     private String password;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "roomname")
+    private Room currentRoom;
+    private boolean ownsRoom;
+
+    public User (String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
