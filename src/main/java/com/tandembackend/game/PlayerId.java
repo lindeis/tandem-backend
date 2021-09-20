02d4660eb7007e6d9d534fe73c437aade7b4eba7
nybah <botosarpad@gmail.com>
2021-09-20 14:20:48 +0200

    Added equals and hashCode functions to PlayerId.

--- src/main/java/com/tandembackend/game/PlayerId.java
+++ src/main/java/com/tandembackend/game/PlayerId.java
@@ -4,10 +4,31 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @NoArgsConstructor
 @AllArgsConstructor
 public class PlayerId implements Serializable {
     private String room;
     private TablePosition position;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        if (o.getClass() != this.getClass()) {
+            return false;
+        }
+        final PlayerId other = (PlayerId) o;
+        if (this.room.equals(other.room) && this.position == other.position) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(room, position);
+    }
 }
