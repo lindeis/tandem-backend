95a8d4e171b8426c282979408b454b8a9886a5ae
nybah <botosarpad@gmail.com>
2021-09-21 11:46:15 +0200

    Added content security policy to http headers and removed the deprecated x-xss-protection header.

--- src/main/java/com/tandembackend/security/SecurityConfiguration.java
+++ src/main/java/com/tandembackend/security/SecurityConfiguration.java
@@ -33,6 +33,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.headers().contentSecurityPolicy("default-src 'self'");
+        http.headers().xssProtection().disable();
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/login").permitAll()
